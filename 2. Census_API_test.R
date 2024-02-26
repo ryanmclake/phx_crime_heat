11d3da5b348ba287217d5e09c50e77715bed84a0
Nate <natebender.mt@gmail.com>
2024-02-26 08:49:31 -0700

    updates to census api script

--- 2. Census_API_test.R
+++ 2. Census_API_test.R
@@ -24,9 +24,10 @@ raw_df <- read_csv(raw_data_path) %>%
   filter(!is.na(occurred_on))
 
 # Read geolocated data
-if (file.exists(geolocated_data_path)) {
-  geo_df <- read_csv(geolocated_data_path) %>% 
-    mutate(zip = as.character(zip))
+if (file.exists(census_geolocated_path)) {
+  geo_df <- read_csv(census_geolocated_path) %>% 
+    mutate(zip = as.character(zip),
+           occurred_on = as.POSIXct(occurred_on, tz = "UTC", format = "%Y-%m-%d %H:%M:%S"))
 } else {
   # Initialize colnames and structure from raw data
   geo_df <- read_csv(raw_data_path) %>%
@@ -38,11 +39,13 @@ if (file.exists(geolocated_data_path)) {
            occurred_to = lubridate::mdy_hm(occurred_to, truncated = 3)) %>%
     filter(!is.na(occurred_on)) %>%
     slice(0)  # Select zero rows to keep just the column structure
-  write_csv(geo_df, geolocated_data_path)
+  write_csv(geo_df, census_geolocated_path)
 }
 
 # Prepare the data frame as before
 records_to_process <- anti_join(raw_df, geo_df, by = c("occurred_on", "100_block_addr", "zip"))
+
+records_to_process <- head(records_to_process, 100)
 
 # Add a unique ID and select necessary columns (adjust column names as necessary)
 records_to_process <- records_to_process %>%
@@ -57,9 +60,6 @@ records_to_process <- records_to_process %>%
          `State` = state,
          `ZIP` = zip) %>%
   select(`Unique ID`, `Street address`, `City`, `State`, `ZIP`)
-
-# Limit to the first 10,000 records for the batch
-records_to_process <- head(records_to_process, 100)
 
 # Export to CSV, ensuring column names match the API's expected format
 write.csv(records_to_process, census_forgeolocation_path, row.names = FALSE, quote = FALSE)
@@ -89,6 +89,10 @@ if (response$status_code == 200) {
   cat("Failed to submit batch geocoding request. Status code:", response$status_code, "\n")
 }
 
+#test <- read_csv(census_temp_path)
+test2 <- read_csv(census_temp_path)
+
+
 col_names <- c("Unique_ID", "Input_Address", "Match_Status", "Match_Type", "Output_Address", "Coordinates", "tiger_id", "side")
 # Read the CSV file with specified column names
 census_geodf <- read_csv(census_temp_path, col_names = col_names)
@@ -100,7 +104,8 @@ census_geodf <- census_geodf %>%
   select(`100_block_addr`, zip, long, lat)
 
 merged_df <- raw_df %>%
-  left_join(census_geodf, by = c("100_block_addr", "zip"))
+  left_join(census_geodf, by = c("100_block_addr", "zip")) %>% 
+  filter(!is.na(lat) & !is.na(long))
 
 
 write_csv(merged_df, census_geolocated_path, col_names = T, append = T)
