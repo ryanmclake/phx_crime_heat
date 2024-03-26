365d72c51e90e88a67eda9214fab48b04be19726
Nate <natebender.mt@gmail.com>
2024-03-25 21:40:58 -0600

    still working on modeling

--- 4_model_predictions.R
+++ 4_model_predictions.R
@@ -1,6 +1,276 @@
-#if(!require(xgboost)) install.packages("xgboost", repos = "http://cran.us.r-project.org")
 library(tidyverse)
 library(here)
+library(yardstick)
+library(fable)
+library(fabletools)
+library(feasts) # for additional diagnostics
+library(tsibble)
+library(fable)
+library(fabletools)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+#install.packages("fable.prophet")
+
+#### SHARED DATA PREP ####
+# *************************
+# *************************
+raw_data_path <- here::here("data/crime_data_raw.csv")
+census_geolocated_path <- here::here("data/census_geolocated.csv") # census API data
+#df_complete_path <- here::here("data/df_complete_forapp.gpkg")
+#blockgroups_geom_path <- here::here("data/blockgroups_geom.gpkg")
+
+raw_df <- readr::read_csv(raw_data_path) %>%
+  rename(block_addr_100 = `100_block_addr`) %>% 
+  mutate(zip = as.character(zip)) 
+
+geo_df <- read_csv(census_geolocated_path) %>%
+  filter(!is.na(lat),
+         !is.na(long),
+         `100_block_addr` != "100_block_addr") %>% 
+  mutate(zip = as.character(zip),
+         lat = as.numeric(lat),
+         long = as.numeric(long)
+  ) %>%
+  rename(block_addr_100 = `100_block_addr`) %>% 
+  distinct(block_addr_100, zip, .keep_all = TRUE) %>% 
+  select(c("block_addr_100", "zip", "lat", "long"))
+
+app_df <- raw_df %>%
+  left_join(geo_df, by = c("block_addr_100", "zip")) %>%
+  select(-state, -occurred_to, -block_addr_100, -grid, -state) %>% 
+  filter(!is.na(lat),!is.na(long)) %>% 
+  slice_sample(n=20000, replace = F)
+
+app_df <- app_df %>%
+  mutate(premise_category = case_when(
+    str_detect(premise_type, "OFFICE|COMMERCIAL|RENTAL STORAGE|FACILITY|WAREHOUSE|FACTORY|PARKING GARAGE") | str_detect(premise_type, "STOREROOM/SHED \\(COMMERCIAL\\)") ~ "Other Commercial",
+    str_detect(premise_type, "SINGLE FAMILY HOUSING|HOUSE|APARTMENT|MOBILE|CONDO|TOWNHOUSE|GARAGE|CARPORT|DRIVEWAY|FENCED RESIDENTIAL YARD|GROUP HOME|HOTEL|MOTEL") | str_detect(premise_type, "STOREROOM/SHED \\(RESIDENTIAL\\)") ~ "Residential",
+    str_detect(premise_type, "GOVERNMENT|PUBLIC|SCHOOL|HOSPITAL|LIGHT RAIL FACILITY|MEDICAL|CHILD CARE|DAY CARE|CHURCH|SYNAGOGUE|TEMPLE|MOSQUE|AIRPORT|COMMUNITY CENTER|NURSING CARE") ~ "Public & Institutional",
+    str_detect(premise_type, "MARKET|STORE|DEPARTMENT|DISCOUNT|RETAIL|FAST FOOD|GAS|GROCERY|SUPER MARKET|BANK|SAVINGS|CREDIT UNION|RESTAURANT|DRUG|SHOPPING MALL|THEATRE|BAR|LOUNGE|NIGHT CLUB|ADULT ONLY|MOVIE") ~ "Commercial & Retail",
+    str_detect(premise_type, "PARK|PLAYGROUND|PARKING LOT|STREET|ROADWAY|SIDEWALK|ALLEY|CONSTRUCTION|OPEN SPACE|DESERT|FIELD|WOODS|FENCED YARD|PARKING GARAGE") ~ "Outdoor & Recreational",
+    str_detect(premise_type, "VEHICLE|BUS STOP|LIGHT RAIL|TRAIN|BUS|FACILITY") ~ "Transport & Utilities",
+    str_detect(premise_type, "UNKNOWN|OTHER|ABANDONED|CONDEMNED") ~ "Miscellaneous",
+    TRUE ~ "Miscellaneous" # Catch-all for anything not matched
+  ))
+
+app_df <- app_df %>%
+  mutate(premise_category = as.factor(premise_category),
+         zip = as.factor(zip),
+         ucr_crime_category = as.factor(ucr_crime_category))
+
+# app_df %>%
+#   group_by(ucr_crime_category) %>%
+#   summarise(Count = n()) %>%
+#   mutate(Frequency_Percentage = (Count / sum(Count)) * 100) %>%
+#   select(ucr_crime_category, Frequency_Percentage) %>% 
+#   arrange(desc(Frequency_Percentage))
+
+
+
+#### MODELING PRACTICE WITH ARIMA ##########
+# *************************
+# *************************
+
+# Step 1: Aggregate data to weekly totals across all zip codes
+df_weekly <- app_df %>%
+  mutate(week_start = as.Date(floor_date(occurred_on, unit = "week"))) %>%
+  group_by(week_start) %>%
+  summarise(total_crimes = n(), .groups = "drop") %>% # Correctly count incidents per week
+  filter(week_start <= "2024-02-11") %>% #filter out an incomplete week
+  as_tsibble(index = week_start)
+
+df_weekly %>%
+  autoplot(total_crimes)
+
+# Step 2: Split into training and test sets (e.g., last 4 observations as test set)
+split_point <- round(nrow(df_weekly) * 0.8)
+train_set <- df_weekly %>% slice(1:split_point)
+test_set <- df_weekly %>% slice((split_point + 1):n())
+library(fable.prophet)
+
+# Step 3: Fit ARIMA and ETS models
+models <- train_set %>%
+  model(
+    #arima_model = ARIMA(total_crimes),
+    #ets_model = ETS(total_crimes),
+    prophet_model = fable.prophet::prophet(total_crimes ~ season(period = 52,
+                                                           order = 2,
+                                                           type = "multiplicative")
+  ))
+
+fitted_models <- models %>% 
+  forecast(h = nrow(test_set))
+
+accuracy_forecasts <- fabletools::accuracy(fitted_models, test_set)
+accuracy_forecasts
+
+models %>% 
+  gg_tsresiduals()
+
+
+
+
+# autoplot(forecasts) +
+#   autolayer(test_set, total_crimes) +
+#   labs(title = "Forecasts vs Actual Data",
+#        x = "Time", y = "Total Crimes",
+#        caption = "Comparison between forecasted values and actual observations.") +
+#   theme_minimal()
+
+totalcrimes_forecast <- df_weekly %>% 
+  model(ETS(total_crimes)) %>% 
+  forecast(h = 10)
+
+# totalcrimes_forecast %>% 
+#   autoplot(df_weekly %>% 
+#              filter(week_start > "2024-01-16"))
+
+totalcrimes_forecast %>% 
+  autoplot(df_weekly %>% 
+             filter(week_start > "2022-01-16"))
+
+
+totalcrimes_forecast <- as_tibble(totalcrimes_forecast) %>% 
+  mutate(total_crimes = .mean,
+         total_crimes_rounded = round(total_crimes)) %>% 
+  select(week_start, total_crimes)
+
+
+## Weekly counts
+weekly_crime_counts <- app_df %>%
+  mutate(week_start = floor_date(occurred_on, unit = "week")) %>%
+  filter(week_start <= "2024-02-11") %>% #filter out an incomplete week
+  count(week_start, ucr_crime_category) %>%
+  complete(week_start, ucr_crime_category, fill = list(n = 0))
+
+weekly_total_crimes <- weekly_crime_counts %>%
+  group_by(week_start) %>%
+  summarise(total_crimes = sum(n))
+
+weekly_crime_fractions <- weekly_crime_counts %>%
+  left_join(weekly_total_crimes, by = "week_start") %>%
+  mutate(fraction = n / total_crimes) %>%
+  select(-total_crimes, -n) %>% 
+  mutate(week_start = as.Date(week_start)) %>% 
+  as_tsibble(index = week_start, key = ucr_crime_category) %>% 
+  fill_gaps() %>%
+  mutate(fraction = replace_na(fraction, 0))
+
+fit <- weekly_crime_fractions %>%
+  model(
+    HL = ETS(fraction ~ error("A") + trend("A") + season("N"))
+  )
+
+crimetypes_forecast <- forecast(fit, h = 4)
+
+forecasts_df <- as_tibble(crimetypes_forecast)
+
+forecasts_df <- forecasts_df %>%
+  group_by(week_start) %>%  
+  mutate(normalized_fraction = .mean / sum(.mean)) %>%
+  ungroup()
+
+fit %>%
+  forecast(h = 4) %>%
+  autoplot(weekly_crime_fractions %>%
+             filter(week_start > "2023-11-01"))
+
+
+## estimate crimes
+crime_estimates <- forecasts_df %>%
+  left_join(totalcrimes_forecast, by = "week_start") %>%
+  mutate(est_crimes = normalized_fraction * total_crimes,
+         est_crimes_rounded = round(est_crimes))
+
+ggplot(crime_estimates, aes(x = week_start, y = est_crimes, color = ucr_crime_category)) +
+  geom_line() +  # Use geom_line() if you want to connect points with lines; replace with geom_point() for scatter plot
+  geom_point() + # Adds the data points; remove if only lines are desired
+  labs(title = "Estimated Crime Types Over Forecast Period",
+       x = "Week Starting",
+       y = "Estimated Number of Crimes Rounded",
+       color = "Crime Type") +
+  theme_minimal() +
+  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + # Adjust date format as needed
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# Modeling by zip code and weekly data ####
+zip_totals <- app_df %>%
+  group_by(zip) %>%
+  summarise(total_crimes = n(), .groups = "drop")
+
+ggplot(zip_totals, aes(x = total_crimes)) +
+  geom_histogram(binwidth = 500, fill = "grey", color = "black") + # Adjust binwidth as needed
+  labs(title = "Distribution of Crimes Across Zip Codes",
+       x = "Total Crimes",
+       y = "Number of Zip Codes") +
+  theme_minimal()
+
+set.seed(123) # Set a seed for reproducibility
+k_means_result <- kmeans(zip_totals$total_crimes, centers = 3)
+
+# Add the cluster assignments to your dataframe
+zip_totals <- zip_totals %>%
+  mutate(cluster = k_means_result$cluster)
+
+# Now you can filter your dataframe by the cluster assignment
+zip_group_1 <- zip_totals %>% filter(cluster == 1)
+zip_group_2 <- zip_totals %>% filter(cluster == 2)
+zip_group_3 <- zip_totals %>% filter(cluster == 3)
+
+# And if you want to visualize the groups on the histogram:
+ggplot(zip_totals, aes(x = total_crimes, fill = factor(cluster))) +
+  geom_histogram(binwidth = 500, alpha = 0.6) +
+  labs(title = "Distribution of Crimes Across Zip Codes with Clusters",
+       x = "Total Crimes",
+       y = "Number of Zip Codes",
+       fill = "Cluster") +
+  theme_minimal()
+
+app_df_group_1 <- app_df %>% 
+  filter(zip %in% zip_group_1$zip)
+
+# 2. Aggregate data weekly
+df_weekly_group_1 <- app_df_group_1 %>%
+  mutate(week_start = as.Date(floor_date(occurred_on, unit = "week"))) %>%
+  group_by(zip, week_start) %>%
+  summarise(total_crimes = n(), .groups = "drop") %>%
+  as_tsibble(index = week_start, key = zip) %>%
+  fill_gaps() %>%
+  mutate(total_crimes = replace_na(total_crimes, 0))
+
+split_point <- round(nrow(df_weekly_group_1) * 0.8)
+
+# Split into training and test sets (based on 80/20 split)
+train_set_group_1 <- df_weekly_group_1 %>% 
+  slice(1:split_point)
+test_set_group_1 <- df_weekly_group_1 %>% 
+  slice((split_point + 1):n())
+
+models_group_1 <- train_set_group_1 %>%
+  model(prophet_model = fable.prophet::prophet(total_crimes))
+
+# Forecast future crime counts
+fitted_models_group_1 <- models_group_1 %>% 
+  forecast(h = nrow(test_set_group_1))
+
+# Evaluate the forecast against the test set
+accuracy_forecasts_group_1 <- accuracy(fitted_models_group_1, test_set_group_1)
+print(accuracy_forecasts_group_1)
+
+# Optionally, visualize the forecast against actual data
+autoplot(fitted_models_group_1) +
+  autolayer(test_set_group_1, total_crimes) +
+  labs(title = "Forecasts vs Actual Data for Zip Group 1",
+       x = "Time", y = "Total Crimes",
+       caption = "Comparison between forecasted values and actual observations.") +
+  theme_minimal()
+
+
+# FUNCTIONS ####
+# *************************
+# *************************
 library(recipes)
 library(ranger)
 #library(xgboost)
@@ -9,14 +279,9 @@ library(themis)
 library(parsnip)
 library(kernlab)
 library(rsample)
-library(yardstick)
 library(keras)
 library(pheatmap)
 
-
-# FUNCTIONS ####
-# *************************
-# *************************
 gen_synthetic_crimes <- function(zip_code_list, predicted_total_crimes_per_zip, historical_data, start_date, end_date) {
   start_date <- as.Date(start_date)
   end_date <- as.Date(end_date)
@@ -69,60 +334,6 @@ date_sequence <- seq.Date(start_date, end_date, by = "day")
 #zip_list <- c("85051", "85021", "85022", "85031", "85013")
 zip_list <- c("85051", "85021")
 
-#### SHARED DATA PREP ####
-# *************************
-# *************************
-raw_data_path <- here::here("data/crime_data_raw.csv")
-census_geolocated_path <- here::here("data/census_geolocated.csv") # census API data
-#df_complete_path <- here::here("data/df_complete_forapp.gpkg")
-#blockgroups_geom_path <- here::here("data/blockgroups_geom.gpkg")
-
-raw_df <- readr::read_csv(raw_data_path) %>%
-  rename(block_addr_100 = `100_block_addr`) %>% 
-  mutate(zip = as.character(zip)) 
-
-geo_df <- read_csv(census_geolocated_path) %>%
-  filter(!is.na(lat),
-         !is.na(long),
-         `100_block_addr` != "100_block_addr") %>% 
-  mutate(zip = as.character(zip),
-         lat = as.numeric(lat),
-         long = as.numeric(long)
-  ) %>%
-  rename(block_addr_100 = `100_block_addr`) %>% 
-  distinct(block_addr_100, zip, .keep_all = TRUE) %>% 
-  select(c("block_addr_100", "zip", "lat", "long"))
-
-app_df <- raw_df %>%
-  left_join(geo_df, by = c("block_addr_100", "zip")) %>%
-  select(-state, -occurred_to, -block_addr_100, -grid, -state) %>% 
-  filter(!is.na(lat),!is.na(long)) %>% 
-  # filter for now until performance is optimized
-  slice_sample(n=50000, replace = F)
-
-app_df <- app_df %>%
-  mutate(premise_category = case_when(
-    str_detect(premise_type, "OFFICE|COMMERCIAL|RENTAL STORAGE|FACILITY|WAREHOUSE|FACTORY|PARKING GARAGE") | str_detect(premise_type, "STOREROOM/SHED \\(COMMERCIAL\\)") ~ "Other Commercial",
-    str_detect(premise_type, "SINGLE FAMILY HOUSING|HOUSE|APARTMENT|MOBILE|CONDO|TOWNHOUSE|GARAGE|CARPORT|DRIVEWAY|FENCED RESIDENTIAL YARD|GROUP HOME|HOTEL|MOTEL") | str_detect(premise_type, "STOREROOM/SHED \\(RESIDENTIAL\\)") ~ "Residential",
-    str_detect(premise_type, "GOVERNMENT|PUBLIC|SCHOOL|HOSPITAL|LIGHT RAIL FACILITY|MEDICAL|CHILD CARE|DAY CARE|CHURCH|SYNAGOGUE|TEMPLE|MOSQUE|AIRPORT|COMMUNITY CENTER|NURSING CARE") ~ "Public & Institutional",
-    str_detect(premise_type, "MARKET|STORE|DEPARTMENT|DISCOUNT|RETAIL|FAST FOOD|GAS|GROCERY|SUPER MARKET|BANK|SAVINGS|CREDIT UNION|RESTAURANT|DRUG|SHOPPING MALL|THEATRE|BAR|LOUNGE|NIGHT CLUB|ADULT ONLY|MOVIE") ~ "Commercial & Retail",
-    str_detect(premise_type, "PARK|PLAYGROUND|PARKING LOT|STREET|ROADWAY|SIDEWALK|ALLEY|CONSTRUCTION|OPEN SPACE|DESERT|FIELD|WOODS|FENCED YARD|PARKING GARAGE") ~ "Outdoor & Recreational",
-    str_detect(premise_type, "VEHICLE|BUS STOP|LIGHT RAIL|TRAIN|BUS|FACILITY") ~ "Transport & Utilities",
-    str_detect(premise_type, "UNKNOWN|OTHER|ABANDONED|CONDEMNED") ~ "Miscellaneous",
-    TRUE ~ "Miscellaneous" # Catch-all for anything not matched
-  ))
-
-app_df <- app_df %>%
-  mutate(premise_category = as.factor(premise_category),
-         zip = as.factor(zip),
-         ucr_crime_category = as.factor(ucr_crime_category))
-
-# app_df %>%
-#   group_by(ucr_crime_category) %>%
-#   summarise(Count = n()) %>%
-#   mutate(Frequency_Percentage = (Count / sum(Count)) * 100) %>%
-#   select(ucr_crime_category, Frequency_Percentage) %>% 
-#   arrange(desc(Frequency_Percentage))
 
 
 #### REGRESSION MODELING ####
@@ -319,126 +530,4 @@ saveRDS(class_fit, file = here(models_path, "class_fit.rds"))
 saveRDS(class_recipe, file = here(models_path, "class_recipe.rds"))
 saveRDS(regr_fit, file = here(models_path, "regr_fit.rds"))
 saveRDS(regr_recipe, file = here(models_path, "regr_recipe.rds"))
-
-
-#### MODELING PRACTICE WITH ARIMA ##########
-# *************************
-# *************************
-#install.packages("fpp3")
-library(tsibble)
-library(fable)
-library(fabletools)
-library(dplyr)
-library(lubridate)
-library(ggplot2)
-
-# Step 1: Aggregate data to weekly totals across all zip codes
-app_df_weekly_total <- app_df %>%
-  mutate(week_start = as.Date(floor_date(occurred_on, unit = "week"))) %>%
-  group_by(week_start) %>%
-  summarise(total_crimes = n(), .groups = "drop") %>% # Correctly count incidents per week
- # filter(week_start <= "2024-02-11") %>% #filter out an incomplete week
-  as_tsibble(index = week_start)
-
-# app_df_weekly_total %>% 
-#   autoplot(total_crimes)
-
-# Step 2: Split into training and test sets (e.g., last 4 observations as test set)
-split_point <- round(nrow(app_df_weekly_total) * 0.8)
-train_set <- app_df_weekly_total %>% slice(1:split_point)
-test_set <- app_df_weekly_total %>% slice((split_point + 1):n())
-
-# Step 3: Fit ARIMA and ETS models
-models <- train_set %>%
-  model(
-    #ARIMA = ARIMA(total_crimes),
-    ETS = ETS(total_crimes)
-  )
-
-forecasts <- models %>% 
-  forecast(h = nrow(test_set))
-
-accuracy_forecasts <- fabletools::accuracy(forecasts, test_set)
-accuracy_forecasts
-
-# autoplot(forecasts) +
-#   autolayer(test_set, total_crimes) +
-#   labs(title = "Forecasts vs Actual Data",
-#        x = "Time", y = "Total Crimes",
-#        caption = "Comparison between forecasted values and actual observations.") +
-#   theme_minimal()
-
-total_fit <- app_df_weekly_total %>% 
-  model(ETS(total_crimes)) %>% 
-  forecast(h = 50)
-
-total_fit %>% 
-  autoplot(app_df_weekly_total %>% 
-             filter(week_start > "2024-01-16"))
-
-total_fit <- as_tibble(total_fit) %>% 
-  mutate(total_crimes = .mean,
-         total_crimes_rounded = round(total_crimes)) %>% 
-  select(week_start, total_crimes)
-
-
-## Weekly counts
-weekly_crime_counts <- app_df %>%
-  mutate(week_start = floor_date(occurred_on, unit = "week")) %>%
-  count(week_start, ucr_crime_category) %>%
-  complete(week_start, ucr_crime_category, fill = list(n = 0))
-
-weekly_total_crimes <- weekly_crime_counts %>%
-  group_by(week_start) %>%
-  summarise(total_crimes = sum(n))
-
-weekly_crime_fractions <- weekly_crime_counts %>%
-  left_join(weekly_total_crimes, by = "week_start") %>%
-  mutate(fraction = n / total_crimes) %>%
-  select(-total_crimes, -n) %>% 
-  mutate(week_start = as.Date(week_start)) %>% 
-  as_tsibble(index = week_start, key = ucr_crime_category) %>% 
-  fill_gaps() %>%
-  mutate(fraction = replace_na(fraction, 0))
-
-fit <- weekly_crime_fractions %>%
-  model(
-    HL = ETS(fraction ~ error("A") + trend("A") + season("N"))
-  )
-
-crimetypes_forecast <- forecast(fit, h = 4)
-
-forecasts_df <- as_tibble(crimetypes_forecast)
-
-forecasts_df <- forecasts_df %>%
-  group_by(week_start) %>%  
-  mutate(normalized_fraction = .mean / sum(.mean)) %>%
-  ungroup()
-
-fit %>%
-  forecast(h = 4) %>%
-  autoplot(weekly_crime_fractions %>%
-             filter(week_start > "2023-11-01"))
-
-# fit %>% 
-#   forecast(h = 4)
-
-
-
-## estimate crimes
-crime_estimates <- forecasts_df %>%
-  left_join(total_fit, by = "week_start") %>%
-  mutate(est_crimes = normalized_fraction * total_crimes,
-         est_crimes_rounded = round(est_crimes))
-
-ggplot(crime_estimates, aes(x = week_start, y = est_crimes, color = ucr_crime_category)) +
-  geom_line() +  # Use geom_line() if you want to connect points with lines; replace with geom_point() for scatter plot
-  geom_point() + # Adds the data points; remove if only lines are desired
-  labs(title = "Estimated Crime Types Over Forecast Period",
-       x = "Week Starting",
-       y = "Estimated Number of Crimes Rounded",
-       color = "Crime Type") +
-  theme_minimal() +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + # Adjust date format as needed
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
